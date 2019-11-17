1985499a85939450016453c7b6eb6a6cc6015b96
Nathaniel Cain <nathaniel2897@gmail.com>
2019-11-16 21:49:29 -0500

    added Router imports to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Home } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
