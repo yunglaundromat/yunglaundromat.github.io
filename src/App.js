667b8e18a495415d8d7f8a05d6b6d2fd5b2df738
Nathaniel Cain <nathaniel2897@gmail.com>
2019-11-16 21:44:14 -0500

    implemented browserrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
 class App extends Component {
 
   render() {
     return (
-      <div> it's working! </div>
+      <div>
+        <Route exact path={`/home`} render={ (routerProps) => < Home routerProps={routerProps} />} />
+      </div>
     )
   }
 }
